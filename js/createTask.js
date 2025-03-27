fe98653ed132c4ffd31534028f41073118d0c2d8
Daniil Dumshenko <dumshenko2003@gmail.com>
2025-03-27 19:26:22 +0100

    added some features

---
+++ js/createTask.js
@@ -0,0 +1,59 @@
+const token = localStorage.getItem("JWT");
+
+if (token != null) {
+    location.href = "../pages/tasks.html";
+}
+
+const signUpForm = document.querySelector("#signUpForm");
+
+const usernameInput = document.querySelector("#username");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+const alertsContainer = document.querySelector("#alerts");
+
+signUpForm.addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    await signUp();
+});
+
+async function signUp() {
+
+    const username = usernameInput.value;
+    const email = emailInput.value;
+    const password = passwordInput.value;
+
+    const signUpRequestBody = {
+        username: username,
+        email: email,
+        password: password,
+    };
+
+    const response = await fetch("http://localhost:8080/sign-up", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(signUpRequestBody),
+    });
+
+    if (response.ok) {
+
+        alertsContainer.innerHTML = `
+            <div class="alert alert-success">
+              Sign Up Successfully!
+            </div>
+        `;
+
+        setTimeout(function () {
+            location.href = "/pages/login.html";
+        }, 2000);
+
+    } else {
+        alertsContainer.innerHTML = `
+            <div class="alert alert-danger">
+              Sign up Failed!
+            </div>
+        `;
+    }
+}
---
+++ js/tasks.js
@@ -0,0 +1,78 @@
+const token = localStorage.getItem("JWT");
+
+if (token == null) {
+    logout();
+}
+
+const logoutButton = document.querySelector("#logoutButton");
+
+logoutButton.addEventListener("click", function() {
+    logout();
+});
+
+function logout() {
+    localStorage.removeItem("JWT");
+    location.href = "/pages/login.html";
+}
+
+const tasksTableBody = document.querySelector("#tasksTableBody");
+
+async function getTasks() {
+    const response = await fetch("http://localhost:8080/tasks", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+        }
+    });
+
+    if (response.ok) {
+
+        const tasks = await response.json();
+
+        let tableBodyHtml = "";
+        for (let i = 0; i < tasks.length; i++) {
+            const task = tasks[i];
+            tableBodyHtml += `
+            <tr id="${task.id}">
+                <td>${task.title}</td>
+                <td>${task.description}</td>
+                <td>${task.status}</td>
+                <td>${task.priority}</td>
+                <td>${task.due_date}</td>
+                <td>
+                    <button class="btn btn-danger" onclick="deleteTask('${task.id}')">Delete</button>
+                </td>
+            </tr>
+        `;
+        }
+
+        tasksTableBody.innerHTML = tableBodyHtml;
+
+    } else {
+        alert("Can not get tasks!");
+
+        logout();
+    }
+}
+
+getTasks();
+
+
+async function deleteTask(taskId) {
+    const response = await fetch(`http://localhost:8080/tasks/${taskId}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+        }
+    });
+
+    if (response.ok) {
+
+        alert("Task deleted successfully.");
+        document.getElementById(taskId).remove();
+    } else {
+        alert("Task was not deleted!");
+    }
+}
